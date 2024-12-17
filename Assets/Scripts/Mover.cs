26d1f723170f7a308089a0228ff7bf503891b475
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-12-17 19:39:07 +0000

    commit

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
 
-
+    [SerializeField] float moveSpeed = 10f;
 
     void Start()
     {
@@ -14,12 +14,11 @@ public class Mover : MonoBehaviour
     void Update()
     {
 
-        float _xValue = Input.GetAxisRaw("Horizontal");
-        float _yValue = Input.GetAxisRaw("Vertical");
-        float _zValue = 0;
+        float _xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float _zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
 
-        transform.Translate(_xValue, _yValue, _zValue);
+        transform.Translate(_xValue, 0, _zValue);
 
-        Debug.Log("_xValue: " + _xValue.ToString() + " | _yValue: " + _yValue.ToString());
+        Debug.Log("_xValue: " + _xValue.ToString() + " | _zValue: " + _zValue.ToString());
     }
 }
