61d4707fbe1353a44003fccea02a737079929ffd
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-12-17 19:17:45 +0000

    Initial commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] float xValue = 0.01f;
+    [SerializeField] float yValue = 0;
+    [SerializeField] float zValue = 0;
+
+    void Start()
+    {
+        Debug.Log("Game has started");
+        Debug.Log("Game has started again");
+    }
+
+
+    void Update()
+    {
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
