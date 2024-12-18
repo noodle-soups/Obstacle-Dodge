f15c38fdf59ffa822b44a983e94432545b87ce85
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-12-18 19:00:50 +0000

    commit

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,18 +7,27 @@ public class Mover : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Game has started");
+        PrintInstruction();
     }
 
 
     void Update()
     {
+        MovePlayer();
+    }
 
+    void PrintInstruction()
+    {
+        Debug.Log("Print something");
+    }
+
+    void MovePlayer()
+    {
         float _xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float _zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
         transform.Translate(_xValue, 0, _zValue);
-
-        Debug.Log("_xValue: " + _xValue.ToString() + " | _zValue: " + _zValue.ToString());
+        //Debug.Log("_xValue: " + _xValue.ToString() + " | _zValue: " + _zValue.ToString());
     }
+
+
 }
