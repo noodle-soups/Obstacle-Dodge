79290ad1ff1daf8c3da2d06f457d1896c93ac257
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-12-19 19:51:33 +0000

    commit

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -3,13 +3,27 @@ using UnityEngine;
 public class Dropper : MonoBehaviour
 {
 
-    [SerializeField] float dropTime = 3;
+    [SerializeField] float timeToDrop = 3;
+    MeshRenderer mr;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
+
+        mr.enabled = false;
+        rb.useGravity = false;
+    }
 
     void Update()
     {
-        if (Time.time > dropTime)
+        if (Time.time > timeToDrop)
         {
             Debug.Log("Drop!");
+            rb.useGravity = true;
+            mr.enabled = true;
         }
 
     }
