8cb20d117bac3a3315b86f010c303edff275bc29
Martin Mallon <88795187+noodle-soups@users.noreply.github.com>
2024-12-28 13:42:56 +0000

    commit

--- Assets/Scripts/EndGameManager.cs
+++ Assets/Scripts/EndGameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndGameManager : MonoBehaviour
 {
@@ -6,12 +7,27 @@ public class EndGameManager : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject endGameUI;
 
+    private void Start()
+    {
+        endGameUI.SetActive(false);
+    }
+
     private void Update()
+    {
+        ActivateEngGameUI();
+    }
+
+    private void ActivateEngGameUI()
     {
         if (player == null)
         {
             endGameUI.SetActive(true);
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
 }
